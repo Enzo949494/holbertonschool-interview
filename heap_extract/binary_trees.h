a7d994a4361d32bc2be60039520f90789405e091
Enzo949494 <9812@holbertonstudents.com>
2026-01-16 10:49:13 +0100

    task0, extract

---
+++ heap_extract/binary_trees.h
@@ -0,0 +1,30 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+/* Prototypes des fonctions que tu vas écrire */
+int heap_extract(heap_t **root);
+size_t binary_tree_size(const binary_tree_t *tree);
+
+#endif /* _BINARY_TREES_H_ */
